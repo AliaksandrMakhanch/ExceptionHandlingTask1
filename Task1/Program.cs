fa9393925abe7f1a446134fe94e455001b35f310
EPAM\Aliaksandr_Makhnach <aliaksandr_makhnach@epam.com>
2023-11-09 14:59:11 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+               Console.WriteLine("Enter symbols. No null input is allowed");
+
+                while (true)
+                {
+                    try
+                    {
+                        string input = Console.ReadLine();
+
+                        PrintFirstCharacter(input);
+                    }
+                    catch (ArgumentException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+
+            static void PrintFirstCharacter(string input)
+            {
+                if (string.IsNullOrEmpty(input))
+                {
+                    throw new ArgumentException("Input cannot be empty");
+                }
+
+                Console.WriteLine($"First character is: {input[0]}");
+            }
+        }
+    }
+
+    
